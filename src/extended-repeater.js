c8cc5cf08d4bee5a94fdfa869fd76f553dc59d10
antonskrebetz <antonskrebetz@mail.ru>
2021-02-21 23:08:41 +0300

    feat: add some ready tasks

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+	let repeat = options.repeatTimes,
+		separ = options.separator,
+		add = options.addition,
+		addTime = options.additionRepeatTimes,
+		addSep = options.additionSeparator;
+	
+	if (!repeat) {repeat = 1;}
+	if (!addTime) {addTime = 1;}
+	if (!separ) {separ = '+';}
+	if (!addSep) {addSep = '|';}
+
+	let strAdd = '';
+	let resultStr = '';
+
+	let j = 1;
+	while (j <= addTime) {
+		strAdd += add + addSep;
+		j++;  
+	}
+
+	let i = 1;
+	while (i <= repeat) {
+		resultStr += str + strAdd + separ;
+		i++;
+	}
+
+	return resultStr;
 };
   
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
-  getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+	arr: [],
+	getLength() {
+		return this.arr.length;
+	},
+	addLink(value) {
+		this.arr.push(`( ${value} )`);
+		return this;
+	},
+	removeLink(position) {
+		if (!position || isNaN(position)) {
+			this.arr = [];
+			throw new Error('put a number');
+		} 
+		this.arr.splice(position - 1, 1);
+		return this;
+	},
+	reverseChain() {
+		this.arr.reverse();
+		return this;
+	},
+	finishChain() {
+		let chain = this.arr.join('~~');
+		this.arr = [];
+		return chain;
+	}
 };
 
 module.exports = chainMaker;
